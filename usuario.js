67447019b609bd9d54d794be97c76bacae499c83
Weslraf <68024752+Weslraf@users.noreply.github.com>
2020-07-23 10:22:28 -0300

    JAVASCRIPT

---
+++ usuario.js
@@ -0,0 +1,17 @@
+function carregarUsuario(){
+
+    var usuarioLogado = localStorage.getItem("user");
+    if(!usuarioLogado){
+        window.location="index.html";
+    }else{
+        var usuarioJson = JSON.parse(usuarioLogado);
+        document.getElementById("dados").innerHTML=
+        "<h3 id='nomeUsuario'>" + usuarioJson.nome + "</h3>" +
+        "<br><h4>" + usuarioJson.email + "<br>" + 
+        "Código: " + usuarioJson.id + "<br></h4>";
+    
+        document.getElementById("foto").innerHTML=
+        "<img width='20%' alt='Imagem não encontrada' src=images/" + usuarioJson.foto + ">";
+    
+    }
+}
